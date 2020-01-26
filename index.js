b4c804032b8e3ac7842cfe6193bc9f6f9c49ba88
Shankar Morwal <shankar@habilelabs.io>
2020-01-26 16:39:24 +0530

    Added responsive web view

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 import React from 'react';
 import {WebView } from 'react-native-webview';
-import {View} from "react-native";
+import {View, Dimensions} from "react-native";
+import AutoHeightWebView from 'react-native-autoheight-webview';
 
 const defaultOptions = {
-	messageStyle: 'none',
-	extensions: ['tex2jax.js'],
-	jax: ['input/TeX', 'output/HTML-CSS'],
-	tex2jax: {
-		inlineMath: [['$', '$'], ['\\(', '\\)']],
-		displayMath: [['$$', '$$'], ['\\[', '\\]']],
-		processEscapes: true,
-	},
-	TeX: {
-		extensions: ['AMSmath.js', 'AMSsymbols.js', 'noErrors.js', 'noUndefined.js']
-	}
+    messageStyle: 'none',
+    extensions: ['tex2jax.js'],
+    jax: ['input/TeX', 'output/HTML-CSS'],
+    tex2jax: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        displayMath: [['$$', '$$'], ['\\[', '\\]']],
+        processEscapes: true,
+    },
+    TeX: {
+        extensions: ['AMSmath.js', 'AMSsymbols.js', 'noErrors.js', 'noUndefined.js']
+    }
 };
 
 class MathJax extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			height: 1
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            height: 1
+        };
+    }
 
-	handleMessage(message) {
-		this.setState({
-			height: Number(message.nativeEvent.data)
-		});
-	}
+    handleMessage(message) {
+        this.setState({
+            height: Number(message.nativeEvent.data)
+        });
+    }
 
-	wrapMathjax(content) {
-		const options = JSON.stringify(
-			Object.assign({}, defaultOptions, this.props.mathJaxOptions)
-		);
+    wrapMathjax(content) {
+        const options = JSON.stringify(
+            Object.assign({}, defaultOptions, this.props.mathJaxOptions)
+        );
 
-		return `
+        return `
 			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 			<script type="text/x-mathjax-config">
 				MathJax.Hub.Config(${options});
@@ -52,24 +53,21 @@ class MathJax extends React.Component {
 				${content}
 			</div>
 		`;
-	}
-	render() {
-		const html = this.wrapMathjax(this.props.html);
+    }
+    render() {
+        const html = this.wrapMathjax(this.props.html);
 
-		// Create new props without `props.html` field. Since it's deprecated.
-		const props = Object.assign({}, this.props, { html: undefined });
-
-		return (
-			<View style={{ height: this.state.height, ...props.style }}>
-				<WebView
-					scrollEnabled={false}
-					onMessage={this.handleMessage.bind(this)}
-					source={{ html }}
-					{...props}
-				/>
-			</View>
-		);
-	}
+        // Create new props without `props.html` field. Since it's deprecated.
+        const props = Object.assign({}, this.props, { html: undefined });
+        return (
+            <AutoHeightWebView
+                source={{html}}
+                scalesPageToFit={true}
+                viewportContent={'width=device-width, user-scalable=no'}
+                {...props}
+            />
+        )
+    }
 }
 
 export default MathJax;
