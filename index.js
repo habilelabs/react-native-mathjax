836904107f03f376be967f404f5879bf9c96efbe
Shankar morwal <shankar@habilelabs.io>
2020-01-26 16:08:14 +0530

    updated index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, WebView } from 'react-native';
+import {WebView } from 'react-native-webview';
+import {View} from "react-native";
 
 const defaultOptions = {
 	messageStyle: 'none',
