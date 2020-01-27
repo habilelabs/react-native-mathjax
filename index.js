65262f132280fefb1d3893944b23992cc4f3dd28
Shankar Morwal <shankar@habilelabs.io>
2020-01-27 23:39:59 +0530

    updated index

--- index.js
+++ index.js
@@ -18,39 +18,52 @@ const defaultOptions = {
 };
 
 function MathJax(props) {
-    const {html, mathJaxOptions} = props;
+    const {html, mathJaxOptions, styles} = props;
+    let isMathJaxPresent = false;
+
     //wrap html in math jaxx.
     function wrapMathjax() {
         const options = JSON.stringify(
             Object.assign({}, defaultOptions, mathJaxOptions)
         );
-        return `
-			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-			<script type="text/x-mathjax-config">
-				MathJax.Hub.Config(${options});
-
-				MathJax.Hub.Queue(function() {
-					var height = document.documentElement.scrollHeight;
-					window.postMessage(String(height));
-					document.getElementById("formula").style.visibility = '';
-				});
-			</script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js"></script>
-			<div id="formula" style="visibility: hidden;">
-				${html}
-			</div>
-		`;
+        //if not mathjax present then we do not need to use mathjax.
+        if (!html || html.indexOf('$$') === -1) {
+            return html;
+        } else {
+            isMathJaxPresent = true;
+            return `<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+                <script type="text/x-mathjax-config">
+                    MathJax.Hub.Config(${options});
+                    MathJax.Hub.Queue(function() {
+                        var height = document.documentElement.scrollHeight;
+                        window.postMessage(String(height));
+                        document.getElementById("formula").style.visibility = '';
+                    });
+                </script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js"></script>
+                <p id="formula" style="visibility: hidden;">
+                    ${html}
+			</p>`;
+        }
     }
+
     const calculatedHtml = wrapMathjax();
     // Create new props without `props.html` field. Since it's deprecated.
-    const calculatedProps = Object.assign({}, props, { html: undefined });
+    const calculatedProps = Object.assign({}, props, {html: undefined});
+    const calculatedStyles = Object.assign({}, {width: Dimensions.get('window').width - 30}, styles);
+    function onSizeUpdated(size) {
+        console.log('------------>>>', size, calculatedStyles);
+    }
     return (
         <AutoHeightWebView
+            style={calculatedStyles}
             source={{html: calculatedHtml}}
             scalesPageToFit={true}
+            onSizeUpdated={onSizeUpdated}
             viewportContent={'width=device-width, user-scalable=no'}
             {...calculatedProps}
         />
     )
 }
+
 export default MathJax;
